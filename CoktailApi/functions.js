38ca536ef51fa095a23c0ac93d8510f72e00cca8
PWMCode <20213TN152@utez.edu.mx>
2025-10-21 15:34:38 -0600

    cambios finales

--- CoktailApi/functions.js
+++ CoktailApi/functions.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log("Service Worker registrado", reg))
+      .catch(err => console.error("Error al registrar el Service Worker", err));
+  });
+}
+
+
 const buscarCotel = async () => {
 
     const coctel = document.getElementById("valor").value
